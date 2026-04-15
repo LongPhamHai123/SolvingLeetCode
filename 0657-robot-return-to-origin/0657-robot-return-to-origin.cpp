8842fe1d4bbb6ef2f98a83e8853f2a44c4cd0adf
LongPhamHai357 <dotrucbk@gmail.com>
2026-04-15 11:19:50 +0700

    N/A (0%) N/A (0%) 0657-robot-return-to-origin .cpp

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.cpp
@@ -4,9 +4,9 @@ public:
         int x=0,y=0;
         for(char c: moves){
             if ( c == 'U') y+=1;
-            if ( c == 'D') y-=1;
-            if ( c == 'L') x-=1;
-            if ( c == 'R') x+=1;
+            else if ( c == 'D') y-=1;
+            else if ( c == 'L') x-=1;
+            else if ( c == 'R') x+=1;
         }
         if (x==0 && y==0) return true;
         return false;
