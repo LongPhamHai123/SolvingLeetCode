eb96207bd872b58d0e22defffb76facb62930f6b
LongPhamHai357 <dotrucbk@gmail.com>
2026-04-18 18:06:05 +0700

    0 ms (100%) 18.1 MB (55.33%) 0733-flood-fill .cpp

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -3,20 +3,27 @@ public:
     void dfs(vector<vector<int>>& image, int sr, int sc, int original_color, int color) {
         int row = image.size();
         int col = image[0].size();
-        if (original_color == color) return;
-        if ( (sr < 0 || sr >= row) || (sc < 0 || sc >= col ){
-            return;
-        }
+
+        // kiểm tra biên
+        if (sr < 0 || sr >= row || sc < 0 || sc >= col) return;
+
+        // kiểm tra màu khác gốc
+        if (image[sr][sc] != original_color) return;
+
+        // tô màu
         image[sr][sc] = color;
-        dfs( image,sr+1,sc, original_color, color);
-        dfs( image,sr-1,sc, original_color, color);
-        dfs( image,sr,sc+1, original_color, color);
-        dfs( image,sr,sc-1, original_color, color);
+
+        // duyệt 4 hướng
+        dfs(image, sr+1, sc, original_color, color);
+        dfs(image, sr-1, sc, original_color, color);
+        dfs(image, sr, sc+1, original_color, color);
+        dfs(image, sr, sc-1, original_color, color);
     }
+
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         int original_color = image[sr][sc];
-        if (original_color == color) return image;
-        dfs(image,sr,sc, original_color, color);
+        if (original_color == color) return image; // tránh vòng lặp vô hạn
+        dfs(image, sr, sc, original_color, color);
         return image;
     }
 
