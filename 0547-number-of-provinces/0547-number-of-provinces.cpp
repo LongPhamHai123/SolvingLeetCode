e96669b1933c3405ddbd55514412356fb4fcb334
LongPhamHai357 <dotrucbk@gmail.com>
2026-04-21 09:41:03 +0700

    N/A (0%) N/A (0%) 0547-number-of-provinces .cpp

--- 0547-number-of-provinces/0547-number-of-provinces.cpp
+++ 0547-number-of-provinces/0547-number-of-provinces.cpp
@@ -1,26 +1,24 @@
 class Solution {
 public:
-    void dfs(vector<vector<int>>& isConnected, vector<bool>& visited, int i) {
+    void dfs(vector<vector<int>>& isConnected, vector<bool> visited, int i) {
         visited[i] = true;
         int n = isConnected.size();
-        for (int j = 0; j < n; j++) {
+        for (int j=0; j < n; j++) {
             if (isConnected[i][j] == 1 && !visited[j]) {
                 dfs(isConnected, visited, j);
             }
         }
     }
-
     int findCircleNum(vector<vector<int>>& isConnected) {
         int n = isConnected.size();
+        int province = 0;
         vector<bool> visited(n, false);
-        int provinces = 0;
-
-        for (int i = 0; i < n; i++) {
+        for (int i=0; i < n; i++) {
             if (!visited[i]) {
                 dfs(isConnected, visited, i);
-                provinces++;
+                province++;
             }
         }
-        return provinces;
+        return province;
     }
 };
